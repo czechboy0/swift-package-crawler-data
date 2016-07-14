e9916492237b946ef8629ad67d3148f0c59d818a
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-07-14 11:39:30 +0200

    Updated data on 2016-07-14 at 11:39

--- PackageSwiftFiles/osjup_SwiftyMailer-Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "SwiftyMailer",
-    dependencies: [
-        .Package(url: "https://github.com/osjup/Perfect-libcurl.git", majorVersion: 0, minor: 5)
-    ]
-)
